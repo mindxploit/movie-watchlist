67b2045fd10736c10ba3e215b4d2a2d6a06f881d
mindxploit <bobbonicola@gmail.com>
2020-08-07 23:33:19 +0200

    Updates

---
+++ precache-manifest.10c2d5a5aa690f68d9504caca1592e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a76267c3b9911f5d0b0f87dfa9e193",
+    "url": "/movie-watchlist/index.html"
+  },
+  {
+    "revision": "a52b2513563df8813bf7",
+    "url": "/movie-watchlist/static/css/main.8465deb4.chunk.css"
+  },
+  {
+    "revision": "58be4c91179dc5b5fdc7",
+    "url": "/movie-watchlist/static/js/2.ec1919da.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movie-watchlist/static/js/2.ec1919da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52b2513563df8813bf7",
+    "url": "/movie-watchlist/static/js/main.96b936d2.chunk.js"
+  },
+  {
+    "revision": "149b9ee4f3782c23eb28",
+    "url": "/movie-watchlist/static/js/runtime-main.fe6d8bad.js"
+  }
+]);
